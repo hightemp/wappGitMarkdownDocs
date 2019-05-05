1e4473cb5a24d692a5f1e721e8d98ccb707c180a
Anton Panov <hightemp.unknown@gmail.com>
2019-05-05 09:34:54 +0300

    update

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
- 
+<!doctype html>
+<html lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="css/main.css">
+
+</head>
+
+<body>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
